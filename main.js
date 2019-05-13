d42468f5e37c0a9b408e7f333c64c55c07b9e857
surxey <50554853+surxey@users.noreply.github.com>
2019-05-13 11:00:01 -0200

    Changed load method to XHR. Changed save timeout to 0

--- main.js
+++ main.js
@@ -1,17 +1,19 @@
 Number.prototype.clamp = function(min, max) {
-  return Math.min(Math.max(this, min), max);
+    return Math.min(Math.max(this, min), max);
 };
 
-var currentTimeout = NaN;
-
+var currentTimeout;
+var c;
+var ctx;
 function start(){
-    var c = document.getElementById('pixelart');
-    let ctx = c.getContext('2d');
-    ctx.moveTo(0,0);
+    c = document.getElementById('pixelart');
+    ctx = c.getContext('2d');
     
-    let img = new Image();
-    img.src = 'saves/save.png';
-    ctx.drawImage(img, 0, 0);
+    let img = new Image;
+    img.onload = function(){
+        ctx.drawImage(img, 0, 0);
+    };
+    img.src = 'http://surx.org/pixjs/saves/save.png';
 
     let px = 5;
     $('#px')[0].value = px;
@@ -21,35 +23,19 @@ function start(){
     
     // When clicked on canvas
     $('#pixelart').click(function(e){
-        px = parseInt($('#px')[0].value).clamp(1, 10);
-        $('#px')[0].value = px;
-        // Read RGB values
-        let r = parseInt($('#r')[0].value).clamp(0, 255);
-        let g = parseInt($('#g')[0].value).clamp(0, 255);
-        let b = parseInt($('#b')[0].value).clamp(0, 255);
-        // Calculate click position
-        let x = e.pageX - $(this).position().left - 1;
-        let y = e.pageY - $(this).position().top - 1;
-        x = x - (x%px);
-        y = y - (y%px);
-        // Fill "pixel"
-        ctx.fillStyle = "rgba("+r+","+g+","+b+","+(255/255)+")";
-        ctx.fillRect(x, y, px, px);
-        //Save image to the server
-        clearTimeout(currentTimeout);
-        currentTimeout = setTimeout(function(){save(c);}, 5000);
+        // Clear canvas
+        ctx.clearRect(0, 0, c.width, c.height);
         // Load image from server
-        let img = new Image();
-        img.src = 'saves/save.png';
-        ctx.drawImage(img, 0, 0);
+	    $('#log')[0].textContent = 'Loading...';
+        load(e);
     });
 };
 
 function save(c) {
     // Convert to encoded Base64 data
 	var dataURL = c.toDataURL('image/png');
-	// Send to page upload_data.php
-	$.ajax({
+    // Send to page upload_data.php
+    $.ajax({
         type: "POST",
         url: "upload_data.php",
         data: {
@@ -57,5 +43,48 @@ function save(c) {
         }
     }).done(function(o) {
         console.log(o);
+        $('#log')[0].textContent = o;
     });
 };
+
+function continueFilling(e){
+    px = parseInt($('#px')[0].value).clamp(1, 10);
+    $('#px')[0].value = px;
+    // Read RGB values
+    let r = parseInt($('#r')[0].value).clamp(0, 255);
+    let g = parseInt($('#g')[0].value).clamp(0, 255);
+    let b = parseInt($('#b')[0].value).clamp(0, 255);
+    // Calculate click position
+    let x = e.pageX - $('#pixelart').position().left - 1;
+    let y = e.pageY - $('#pixelart').position().top - 1;
+    x = x - (x%px);
+    y = y - (y%px);
+    // Fill "pixel"
+    ctx.fillStyle = "rgba("+r+","+g+","+b+","+(255/255)+")";
+    ctx.fillRect(x, y, px, px);
+    //Save image to the server
+    clearTimeout(currentTimeout);
+    currentTimeout = setTimeout(function(){save(c);}, 0);
+}
+
+function load(e) {
+    var req = new XMLHttpRequest();
+    //req.withCredentials = true;
+    req.onload = function() {
+            var reader = new FileReader();
+            reader.onloadend = function() {
+                let img = new Image;
+                img.onload = function(){
+                    ctx.drawImage(img, 0, 0);
+                    continueFilling(e);
+	            };
+                img.src = reader.result;
+            };
+            reader.readAsDataURL(req.response);
+
+    };
+    req.open("GET", 'http://surx.org/pixjs/saves/save.png', true);
+    req.responseType = 'blob';
+    req.send();    
+}
+
